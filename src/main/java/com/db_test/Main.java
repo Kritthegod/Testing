d8456f94dc475c75d722a60eda414f331116d78c
Kritthegod <142289388+Kritthegod@users.noreply.github.com>
2025-05-14 11:32:17 +0630

    First Commit

--- src/main/java/com/db_test/Main.java
+++ src/main/java/com/db_test/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         //This is output.
-        System.out.printf("Hello World");
+        System.out.print("Hello World");
 
     }
 }
