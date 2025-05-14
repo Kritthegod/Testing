49961962553bcff5967492713dcf190993004b72
Kritthegod <kyawzayarthein15@gmail.com>
2025-05-14 11:30:04 +0630

    First Commit

---
+++ src/main/java/com/db_test/Main.java
@@ -0,0 +1,18 @@
+package com.db_test;
+
+/**
+    This is the testing Main CLass.
+    Created by:----
+    Created Date:----
+ */
+public class Main {
+
+    /**
+
+     */
+    public static void main(String[] args) {
+        //This is output.
+        System.out.printf("Hello World");
+
+    }
+}
